42cc36a54126471992fc3fd9a0230d8cc2cb8bc6
Kyulee Jeon <97151697+kyulee-jeon@users.noreply.github.com>
2024-06-26 09:33:24 +0900

    Update save_to_csv.py

--- save_to_csv.py
+++ save_to_csv.py
@@ -2,7 +2,7 @@ import csv
 
 def save_to_csv(header_info_list, output_file):
     with open(output_file, 'w', newline='', encoding='utf-8') as output:
-        fieldnames = ["File Name", "SOP Class UID", "Tag", "Attribute Name", "Value"]
+        fieldnames = ["File Name", "SOP Class UID", "Study UID", "Series UID", "Tag", "Attribute Name", "Value"]
         dict_writer = csv.DictWriter(output, fieldnames=fieldnames, quoting=csv.QUOTE_ALL)
         dict_writer.writeheader()
         for header_info in header_info_list:
@@ -10,6 +10,8 @@ def save_to_csv(header_info_list, output_file):
                 row = {
                     "File Name": header_info["File Name"],
                     "SOP Class UID": header_info["SOP Class UID"],
+                    "Study UID": header_info["Study UID"],
+                    "Series UID": header_info["Series UID"],
                     "Tag": item["Tag"],
                     "Attribute Name": item["Attribute Name"],
                     "Value": item["Value"]
