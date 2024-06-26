9010571285438aea79132dbb8127a417723850ee
Kyulee Jeon <97151697+kyulee-jeon@users.noreply.github.com>
2024-06-26 16:36:26 +0900

    Add files via upload

---
+++ dicom-header-extractor.exe/ForMacUser/save_to_csv.py
@@ -0,0 +1,19 @@
+import csv
+
+def save_to_csv(header_info_list, output_file):
+    with open(output_file, 'w', newline='', encoding='utf-8') as output:
+        fieldnames = ["File Name", "SOP Class UID", "Study UID", "Series UID", "Tag", "Attribute Name", "Value"]
+        dict_writer = csv.DictWriter(output, fieldnames=fieldnames, quoting=csv.QUOTE_ALL)
+        dict_writer.writeheader()
+        for header_info in header_info_list:
+            for item in header_info["Header"]:
+                row = {
+                    "File Name": header_info["File Name"],
+                    "SOP Class UID": header_info["SOP Class UID"],
+                    "Study UID": header_info["Study UID"],
+                    "Series UID": header_info["Series UID"],
+                    "Tag": item["Tag"],
+                    "Attribute Name": item["Attribute Name"],
+                    "Value": item["Value"]
+                }
+                dict_writer.writerow(row)
