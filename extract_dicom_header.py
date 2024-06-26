f217461970ba8d28b8deda39be7fd16ef11f3980
Kyulee Jeon <97151697+kyulee-jeon@users.noreply.github.com>
2024-06-26 09:31:07 +0900

    Update extract_dicom_header.py

--- extract_dicom_header.py
+++ extract_dicom_header.py
@@ -6,6 +6,14 @@ def extract_dicom_header(dicom_file):
         sop_class_uid = ds[(0x0008, 0x0016)].value
     else:
         sop_class_uid = 'Unknown SOP Class UID'
+    if ds.StudyInstanceUID:
+        study_uid = ds[(0x0020, 0x000d)].value
+    else:
+        study_uid = 'Unknown Study Instance UID'
+    if ds.SeriesInstanceUID:
+        series_uid = ds[(0x0020, 0x000e)].value
+    else:
+        series_uid = 'Unknown Series Instance UID'
     header_info = []
     for elem in ds:
         if elem.tag.group != 0x7FE0:  # Exclude pixel data
@@ -15,4 +23,4 @@ def extract_dicom_header(dicom_file):
                 "Attribute Name": elem.keyword,
                 "Value": str(elem.value)
             })
-    return header_info, str(sop_class_uid)
+    return header_info, str(sop_class_uid), str(study_uid), str(series_uid)
